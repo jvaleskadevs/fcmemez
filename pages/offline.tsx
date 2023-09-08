39018227f9f8a2ff54fc0a7ca3286d0521db2221
jvaleskadevs <vmjune2019@gmail.com>
2023-09-08 15:56:20 +0200

    fix pwa

--- pages/offline.tsx
+++ pages/offline.tsx
@@ -1,7 +1,11 @@
-export const Offline() {
+import type { NextPage } from 'next';
+
+const Offline: NextPage = () => {
   return (
     <div className="flex flex-col items-center">
       <p>You are offline. Connect to the Internet and try again.</p>
     </div>
   );
 }
+
+export default Offline;
